8a9cd83cd8bd01f00b84cfdbf91163e1af89773b
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2026-01-10 12:05:32 +0100

    init

---
+++ source/main.cpp
@@ -0,0 +1,25 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "GD", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Error creating window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
