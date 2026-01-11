55590b137f1d488f1f188db9c55080ca0f297063
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2026-01-11 13:08:37 +0100

    game integration into engine and delta time output each frame

---
+++ source/Game.cpp
@@ -0,0 +1,15 @@
+#include "Game.h"
+#include <iostream>
+
+bool Game::Init()
+{
+	return true;
+}
+void Game::Update(float deltaTime)
+{
+	std::cout << "Current delta: " << deltaTime << std::endl;
+}
+void Game::Destroy()
+{
+
+}
